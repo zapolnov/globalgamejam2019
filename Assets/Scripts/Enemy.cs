2cf56adfd038ccad97a304c997b51067d2a3ca0d
Nikolay Zapolnov <zapolnov@gmail.com>
2019-01-26 00:06:13 +0200

    Started to implement enemy.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public sealed class Enemy : MonoBehaviour
+{
+    public EnemyGroundDetector LeftGroundDetector;
+    public EnemyGroundDetector RightGroundDetector;
+
+    public float Speed = 0.1f;
+    public float WaitTime = 1.0f;
+    public bool MovesRight;
+
+    private float mWaiting;
+
+    void Update()
+    {
+        if (mWaiting > 0.0f) {
+            mWaiting -= Time.deltaTime;
+            if (mWaiting > 0.0f)
+                return;
+        }
+
+        if (LeftGroundDetector.IsOnGround || RightGroundDetector.IsOnGround) {
+            if (!LeftGroundDetector.IsOnGround && !MovesRight) {
+                MovesRight = true;
+                mWaiting = WaitTime;
+            } else if (!RightGroundDetector.IsOnGround && MovesRight) {
+                MovesRight = false;
+                mWaiting = WaitTime;
+            } else {
+                Vector3 pos = transform.position;
+                pos.x += (MovesRight ? Speed : -Speed);
+                transform.position = pos;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyGroundDetector.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public sealed class EnemyGroundDetector : MonoBehaviour
+{
+    public bool IsOnGround => mIsOnGround != 0;
+    private int mIsOnGround;
+
+    //void OnCollisionEnter2D(Collision collision)
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        ++mIsOnGround;
+    }
+
+    //void OnCollisionExit2D(Collision collision)
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        --mIsOnGround;
+    }
+}
