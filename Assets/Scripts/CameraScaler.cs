a791b47eec6a314fa6b7b68ffb1f6f804213303d
Nikolay Zapolnov <zapolnov@gmail.com>
2019-01-25 23:19:36 +0200

    Scale screen to always have the same in-game width.

---
+++ Assets/Scripts/CameraScaler.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public sealed class CameraScaler : MonoBehaviour
+{
+    public float WidthToBeSeen = 640.0f; 
+
+    void Update()
+    {
+        Camera.main.orthographicSize = WidthToBeSeen * Screen.height / Screen.width * 0.5f;
+    }
+}
