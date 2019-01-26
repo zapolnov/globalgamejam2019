661067c8b2efee09096ab49ecb1692a44c535bd1
Nikolay Zapolnov <zapolnov@gmail.com>
2019-01-27 00:21:01 +0200

    Heart icons in the HUD.

--- Assets/Scripts/UI/LifeBar.cs
+++ Assets/Scripts/UI/LifeBar.cs
@@ -21,8 +21,8 @@ public sealed class LifeBar : MonoBehaviour
             rt.anchorMin = new Vector2(1, 0);
             rt.anchorMax = new Vector2(1, 1);
             rt.pivot = new Vector2(1, 0.5f);
-            rt.offsetMin = new Vector2(-25.0f * (Lives.Count + 1), 5.0f);
-            rt.offsetMax = new Vector2(-5.0f - 25.0f * (Lives.Count), -5.0f);
+            rt.offsetMin = new Vector2(-60.0f * (Lives.Count + 1), 5.0f);
+            rt.offsetMax = new Vector2(-5.0f - 60.0f * (Lives.Count), -5.0f);
             Lives.Push(image);
         }
 
