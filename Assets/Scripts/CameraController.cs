be6a6ddb82a48c3c6d77515447fcfe48d57afaca
Nikolay Zapolnov <zapolnov@gmail.com>
2019-01-26 14:22:33 +0200

    Camera follows the player.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public sealed class CameraController : MonoBehaviour
+{
+    public float Speed = 1.0f;
+    public Player Player;
+
+    public void LateUpdate()
+    {
+        Vector3 position = transform.position;
+
+        float step = Player.transform.position.y - position.y;
+        float maxStep = Time.deltaTime * Speed;
+        if (Mathf.Abs(step) > Mathf.Abs(maxStep))
+            step = maxStep * Mathf.Sign(step);
+
+        position.y += step;
+        transform.position = position;
+    }
+}
