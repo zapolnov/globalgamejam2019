fd4091688bc2ba2e05ba460705cba46f9b8c6d48
Nikolay Zapolnov <zapolnov@gmail.com>
2019-01-26 18:00:03 +0200

    Proper intro screen.

---
+++ Assets/Scripts/UI/BlinkingText.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class BlinkingText : MonoBehaviour
+{
+    private Text mText;
+    void Awake()
+    {
+        mText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        Color c = mText.color;
+        c.a = Mathf.Abs(1.0f - Time.timeSinceLevelLoad % 2.0f);
+        mText.color = c;
+    }
+}
